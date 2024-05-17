277f35b74041fb0a88b051bc7b3120feb340fdb4
Faakhir <zahidfaakhir@gmail.com>
2024-05-17 15:18:19 +0500

    update

--- maps/app/Http/Controllers/CityController.php
+++ maps/app/Http/Controllers/CityController.php
@@ -16,59 +16,33 @@ class CityController extends Controller
     {
         // Get the coordinates (latitude and longitude) of the input city
         $inputCity = City::where('city', $cityName)->first();
-        $inputLatitude = deg2rad($inputCity->latitude); // Convert degrees to radians
-        $inputLongitude = deg2rad($inputCity->longitude); // Convert degrees to radians
-    
-        // Find the 5 closest cities based on the input coordinates using the aggregation framework
-        $closestCities = City::raw(function ($collection) use ($inputLatitude, $inputLongitude, $cityName) {
-            return $collection->aggregate([
-                [
-                    '$match' => ['city' => ['$ne' => $cityName]] // Exclude the input city
-                ],
-                [
-                    '$addFields' => [
-                        'distance' => [
-                            '$multiply' => [
-                                6371, // Earth's radius in kilometers
-                                [
-                                    '$acos' => [
-                                        [
-                                            '$add' => [
-                                                [
-                                                    '$multiply' => [
-                                                        ['$sin' => $inputLatitude],
-                                                        ['$sin' => ['$degreesToRadians' => '$latitude']]
-                                                    ]
-                                                ],
-                                                [
-                                                    '$multiply' => [
-                                                        ['$cos' => $inputLatitude],
-                                                        ['$cos' => ['$degreesToRadians' => '$latitude']],
-                                                        ['$cos' => [
-                                                            '$subtract' => [
-                                                                ['$degreesToRadians' => $inputLongitude],
-                                                                ['$degreesToRadians' => '$longitude']
-                                                            ]
-                                                        ]]
-                                                    ]
-                                                ]
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
+        $inputLatitude = $inputCity->latitude; // Convert degrees to radians
+        $inputLongitude = $inputCity->longitude; // Convert degrees to radians
+ // Find the 5 closest cities based on the input coordinates using the aggregation framework
+$closestCities = City::raw(function ($collection) use ($inputLatitude, $inputLongitude, $cityName) {
+    return $collection->aggregate([
+        [
+            '$match' => ['city' => ['$ne' => $cityName]] // Exclude the input city
+        ],
+        [
+            '$addFields' => [
+                'distance' => [
+                        '$add' => [
+                            ['$pow' => [['$subtract' => ['$latitude', $inputLatitude]], 2]],
+                            ['$pow' => [['$subtract' => ['$longitude', $inputLongitude]], 2]]
                         ]
-                    ]
-                ],
-                [
-                    '$sort' => ['distance' => 1] // Sort by distance in ascending order
-                ],
-                [
-                    '$limit' => 5 // Limit the result to 5 closest cities
                 ]
-            ]);
-        });   
-        return $closestCities;
+            ]
+        ],
+        [
+            '$sort' => ['distance' => 1] // Sort by distance in ascending order
+        ],
+        [
+            '$limit' => 5 // Limit the result to 5 closest cities
+        ]
+    ]);
+});   
+return $closestCities;
     }
 
     public function getCityNamesThatStartWith($initials)
